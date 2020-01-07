713a3d2a72564f7b959be4b60c1b7ef1a2a80b5e
Hans Neefs <hans.neefs@unifiedpost.com>
2020-01-07 14:13:29 +0100

    database

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,17 +56,16 @@ resource "aws_security_group" "devotest" {
   }
 }
 
-resource "aws_ebs_volume" "awx" {
-  availability_zone = aws_instance.devotest.availability_zone
-  size              = 50
-
+resource "aws_ebs_volume" "tmp" {
+  availability_zone = aws_instance.devotest_web.availability_zone
+  size              = 10
   tags = {
     Name = "tmp"
   }
 }
 
 
-resource "aws_instance" "devotest" {
+resource "aws_instance" "devotest_web" {
   ami = var.aws_ami_id
   instance_type = "t2.micro"
   key_name = var.key_name
@@ -80,41 +79,75 @@ resource "aws_instance" "devotest" {
               #!/bin/bash
               yum update -y
               amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-              yum install -y httpd mariadb-server
+              yum install -y httpd
               systemctl start httpd
               systemctl enable httpd
-              systemctl start mariadb
-              systemctl enable mariadb
               usermod -a -G apache apache
               chown -R apache:apache /var/www
               chmod 2775 /var/www
               find /var/www -type d -exec chmod 2775 {} \;
               find /var/www -type f -exec chmod 0664 {} \;
-              echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+   EOF
+
+}
+
+resource "aws_instance" "devotest_db" {
+  ami = var.aws_ami_id
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  #vpc_security_group_ids = ["sg-0d9ecfc76cc0661c9"]
+  vpc_security_group_ids = [aws_security_group.devotest.id]
+  tags = {
+    Name = "terraform-devotest"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+              yum install -y mariadb-server
+              systemctl start mariadb
+              systemctl enable mariadb
    EOF
 
 }
 
 resource "aws_volume_attachment" "tmp_attachement" {
   device_name  = "/dev/xvdb"
-  instance_id  = aws_instance.devotest.id
-  volume_id    = aws_ebs_volume.awx.id
+  instance_id  = aws_instance.devotest_web.id
+  volume_id    = aws_ebs_volume.tmp.id
   # skip_destroy = "true"
 }
 
 
-output "public_ip" {
-  value       = aws_instance.devotest.public_ip
+output "public_ip_web" {
+  value       = aws_instance.devotest_web.public_ip
   description = "The public IP of the web server"
 }
 
-output "name" {
-  value       = aws_instance.devotest.tags.Name
+output "name_web" {
+  value       = aws_instance.devotest_web.tags.Name
   description = "The Name of the web server"
 }
 
-output "state" {
-  value       = aws_instance.devotest.instance_state
+output "state_web" {
+  value       = aws_instance.devotest_web.instance_state
   description = "The state of the web server"
+}
+
+
+output "public_ip_db" {
+  value       = aws_instance.devotest_db.public_ip
+  description = "The public IP of the db server"
+}
+
+output "name_db" {
+  value       = aws_instance.devotest_db.tags.Name
+  description = "The Name of the db server"
+}
+
+output "state_db" {
+  value       = aws_instance.devotest_db.instance_state
+  description = "The state of the db server"
 }
 
